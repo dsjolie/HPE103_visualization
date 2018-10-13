337ad7e39b1e6dc371c33030bee39248cc3b807b
Daniel Sjölie <dsjolie@gmail.com>
2018-10-13 23:23:49 +0200

    Animation to highlight different values.

--- script.js
+++ script.js
@@ -39,6 +39,8 @@ var height = 300;
 
 createDosageVis(vis);
 
+setInterval(doAnimations,33);
+
 function createDosageVis(container) {
 
     var vis = container.append("div").classed("visualization", true);
@@ -77,32 +79,38 @@ function buildVisualizationSVG(vis) {
 
     // Before
     var before_g0 = svg.append("g")
-        .attr("transform", "translate(0,0)");
+        .attr("transform", "translate(20,0)");
     before_g0.append("image")
         .attr("href", "https://cdn.glitch.com/d2a63701-a576-4584-9ecd-f00c9d5f4529%2FAmpull-4.jpg?1539073731810")
         .attr("height", "100")
-        .attr("x", "-20");
+        .attr("x", "-30")
+        .attr("y", "7");
+    before_g0.append("line")
+        .attr("x1", 0)
+        .attr("x2", 40)
+        .attr("y1", 48)
+        .attr("y2", 48)
+        .attr("stroke", "black");
+
     var before_g = before_g0.append("g")
-        .attr("transform", "translate(40,0)");
+        .attr("transform", "translate(20,0)");
     before_g.append("rect").classed("strength before_calc", true)
-        .attr("width", 100)
-        .attr("height", 100)
-        .attr("rx", 10)
-        .attr("y", 0)
-        .attr("x", 0);
+        .attr("width", 140)
+        .attr("height", 20)
+        .attr("x", 20)
+        .attr("y", 80);
     before_g.append("rect").classed("strength before", true)
-        .attr("width", 80)
-        .attr("height", 80)
-        .attr("x", 10)
-        .attr("y", 10)
-        .attr("rx", 10);
+        .attr("width", 140)
+        .attr("height", 10)
+        .attr("x", 20)
+        .attr("y", 85);
     before_g.append("rect").classed("volume before", true)
-        .attr("width", 40)
-        .attr("x", 30)
+        .attr("width", 20)
+        .attr("x", 0)
         .attr("fill", "blue");
     before_g.append("rect").classed("dosage before", true)
-        .attr("width", 20)
-        .attr("x", 40)
+        .attr("width", 10)
+        .attr("x", 5)
         .attr("fill", "red");
     // Spading
     var spadning_g0 = svg.append("g")
@@ -112,13 +120,12 @@ function buildVisualizationSVG(vis) {
         .attr("height", "100")
         .attr("x", "-20");
     var spadning_g = spadning_g0.append("g")
-        .attr("transform", "translate(40,0)");
+        .attr("transform", "translate(10,0)");
     spadning_g.append("rect").classed("strength spadning", true)
         .attr("width", 100)
-        .attr("height", 100)
-        .attr("rx", 15)
+        .attr("height", 20)
         .attr("fill", getDosageColor(0))
-        .attr("x", 0);
+        .attr("x", 70);
     spadning_g.append("rect").classed("volume spadning", true)
         .attr("width", 40)
         .attr("x", 30)
@@ -174,14 +181,6 @@ function buildVisualizationSVG(vis) {
         .attr("y2", 0)
         .attr("stroke", "white")
         .attr("stroke-opacity", "0.5");
-    svg.append("text")
-        .attr("x", 85)
-        .attr("y", 115)
-        .text("+");
-    svg.append("text")
-        .attr("x", 180)
-        .attr("y", 115)
-        .text("=");
 }
 
 function createSetting(vis, label, key, key2) {
@@ -278,6 +277,21 @@ function updateDosageVis() {
         })
         .attr("y", function (d) { return 100 - 50 * Math.min(d.dos / d.max_dos, 1); });
 
+}
+
+function doAnimations()
+{
+    var svg = d3.selectAll("svg");
+
+    var pos = Math.sin(5*window.performance.now()/1000);
+    var amp = 6;
+    var max_height=20;
+    var padding=2;
+    var base_y = 80;
+    // Before
+    var str_bg = svg.selectAll(".strength.before")
+        .attr("height", function (d) { return max_height-amp*(pos+1)-2*padding; }) // pos+1 : -1 - 1 => 0 - 2
+        .attr("y", function (d) { return base_y+padding+amp*(pos+1)*0.5; });
 }
 
 function rgb(r, g, b) {
